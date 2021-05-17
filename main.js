e1fd61e91c10108f02e95c06b074875cb74d4fc8
unknown <wmigue@gmail.com>
2021-05-17 20:36:15 -0300

    10mo commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const funcion = (msg)=>{
     if(msg) nombre = prompt(msg)
     else nombre = prompt("introduce tu nombre")
-    console.log("Su nombre es: "nombre)
+    console.log("Su nombre es: " + nombre)
 }
 
 funcion()
